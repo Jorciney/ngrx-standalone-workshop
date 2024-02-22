3bde9bb4f71c43949c435f57d6fc5f671b6be754
Jorciney <jorciney.dias@hotmail.com>
2024-02-22 12:17:46 +0100

    M7 router store

--- apps/ngrx-workshop/src/main.ts
+++ apps/ngrx-workshop/src/main.ts
@@ -10,15 +10,20 @@ import {provideStoreDevtools} from "@ngrx/store-devtools";
 import {provideEffects} from "@ngrx/effects";
 import * as handleFetchError from "./app/error.effects";
 import {ProductEffects} from "./app/product/+state/product.effects";
-import {CART_FEATURE_KEY, cardReducer} from "./app/cart/+state/cart.reducer";
+import {cardReducer, CART_FEATURE_KEY} from "./app/cart/+state/cart.reducer";
 import * as cartEffects from "./app/cart/+state/cart.effects";
+
+import {ROUTER_FEATURE_KEY} from "./app/router/router.selectors";
+import {provideRouterStore, routerReducer} from "@ngrx/router-store";
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
     provideHttpClient(), provideAnimations(),
     provideStore({product: productReducer}),
-    provideState(CART_FEATURE_KEY,cardReducer ),
+    provideState(CART_FEATURE_KEY, cardReducer),
     provideEffects(ProductEffects, handleFetchError, cartEffects),
+    provideState(ROUTER_FEATURE_KEY, routerReducer),
+    provideRouterStore({stateKey: ROUTER_FEATURE_KEY}),
     provideStoreDevtools()],
 });
